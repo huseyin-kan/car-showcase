0518b18aedc5f0a8162f871e891f3168c62c5439
Huseyin <huseyin.kann@hotmail.com>
2023-11-13 14:06:37 +0300

    Layout Completed

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps{
     title: String,
     containerStyles?: String,
-    handleClick?:MouseEventHandler<HTMLButtonElement>
+    handleClick?:MouseEventHandler<HTMLButtonElement>,
+    btnType?:"button" | "submit",
 
 }
