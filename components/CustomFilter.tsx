f564fe8c4baec135bc39eb1f324e3d9aa4445c8f
Huseyin <huseyin.kann@hotmail.com>
2023-11-13 14:53:20 +0300

    Searchbar started

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CustomFilter = ({title}:any) => {
+    const titles = title
+  return (
+    <div>CustomFilter</div>
+  )
+}
+
+export default CustomFilter
---
+++ components/SearchBar.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useState } from 'react'
+import { SearchManufacturer } from '.';
+
+const SearchBar = () => {
+    const [manufacturer,setManufacturer] = useState('')
+    const handleSearch = ()=>{
+
+    }
+  return (
+    <form onSubmit={handleSearch} className='searchbar'>
+        <div className='searchbar__item'>
+            <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer}/>
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
