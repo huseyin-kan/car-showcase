4af37045fe1400c16ee8a1884ba802e5ad4545a8
Huseyin <huseyin.kann@hotmail.com>
2023-11-20 17:10:54 +0300

    car card completed

--- types/index.ts
+++ types/index.ts
@@ -2,14 +2,32 @@ import { MouseEventHandler } from "react";
 
 
 export interface CustomButtonProps{
-    title: String,
-    containerStyles?: String,
-    handleClick?:MouseEventHandler<HTMLButtonElement>,
-    btnType?:"button" | "submit",
+    title: string;
+    containerStyles?: string;
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
+    btnType?:"button" | "submit";
+    textStyles?:string;
+    rightIcon?:string;
+    isDisabled?:boolean;
 
 }
 
 export interface SearchManufacturerProps{
-    manufacturer:string,
-    setManufacturer:(manufacturer:string) => void,
+    manufacturer:string;
+    setManufacturer:(manufacturer:string) => void;
+}
+
+export interface CarProps{
+    class:string;
+    city_mpg:number;
+    combination_mpg:number;
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number;
+    make:string;
+    model:string;
+    transmission:string;
+    year:number;
 }
